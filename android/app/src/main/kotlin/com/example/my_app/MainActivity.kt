ed8d0a533c650c192b58c40669ceea2595993eb7
moradmohaamed038-pixel <moradmohaamed038@gmail.com>
2026-08-07 16:51:12 +0300

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/my_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/my_app/MainActivity.kt
@@ -1,5 +1,6 @@
-package com.example.my_app
+package com.example.morad_tk_app
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity() {
+}
